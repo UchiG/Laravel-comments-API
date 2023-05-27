2f21c5f025ce7901084e968b9b88a7383697aee0
Uchirai <uchirai.govere@gmail.com>
2023-05-27 21:49:54 +0200

    updated comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    use HasFactory;
+    protected $fillable = ['post_id', 'author_name', 'author_email', 'content'];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CommentController;
+
+Route::post('/comments', [CommentController::class, 'store']);
+Route::get('/comments', [CommentController::class, 'index']);
 
 /*
 |--------------------------------------------------------------------------
